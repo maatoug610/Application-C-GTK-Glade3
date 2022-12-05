e246a1fae7ebd5c21761d565e4abb485569ec0b2
maatoug610 <maatoug610@gmail.com>
2022-12-06 00:08:37 +0100

    version1

--- main.c
+++ main.c
@@ -3,12 +3,23 @@
 #include <string.h>
 
 //Global var
+
+//Discount var
 GtkWidget *price_input;
 GtkWidget *discount_input;
 GtkWidget *result_label;
 
+//Tip var
+GtkWidget *bill_input;
+GtkWidget *tax_input;
+GtkWidget *tip_input;
+GtkWidget *split_input;
+GtkWidget *result_tip;
+
 GtkBuilder *builder;
 GtkWidget *window;
+GtkWidget *windowTip;
+
 
 void discount_calculate(){
 
@@ -35,14 +46,46 @@ void discount_calculate(){
 
 }
 
+void tipCalculator(){
+
+printf("Tip");
+    char *bill;
+    char *tax;
+    char *tip;
+    char *split;
+    char result[20];
+
+    bill = gtk_entry_get_text(bill_input);
+    tax = gtk_entry_get_text(tax_input);
+    tip = gtk_entry_get_text(tip_input);
+    split = gtk_entry_get_text(split_input);
+
+    float b = strtof(bill,NULL);
+    float ta = strtof(tax,NULL);
+    float ti = strtof(tip,NULL);
+    float s = strtof(split,NULL);
+    //operation:
+
+
+    gcvt((b + b*ta/100 + b*ti/100) / s,6,result);
+
+    gtk_label_set_text(GTK_LABEL(result_tip),result);
+
+
+    printf("%s",result);
+
+}
+
 //open App1:
 void open_app1(){
+
 
     builder = gtk_builder_new();
 
     gtk_builder_add_from_file(builder, "app1UI.glade", NULL);
     window = GTK_WIDGET(gtk_builder_get_object(builder, "app1"));
     gtk_builder_connect_signals(builder, NULL);
+
 
 
     /*Event Handler on Button*/
@@ -60,6 +103,33 @@ void open_app1(){
     g_signal_connect(G_OBJECT(g_btn_submit), "clicked", G_CALLBACK(discount_calculate), NULL);
 
     gtk_widget_show_all (window);
+    gtk_main ();
+}
+
+//open Tip
+void open_btn(){
+
+
+    builder = gtk_builder_new();
+
+    gtk_builder_add_from_file(builder, "tipUI.glade", NULL);
+    windowTip = GTK_WIDGET(gtk_builder_get_object(builder, "tip_window"));
+    gtk_builder_connect_signals(builder, NULL);
+
+     /*Event Handler on Button*/
+    GtkWidget *g_btn_cal_tip;
+    g_btn_cal_tip = GTK_WIDGET(gtk_builder_get_object(builder, "btn_tip"));
+
+
+    bill_input = GTK_WIDGET(gtk_builder_get_object(builder, "bill_input"));
+    tax_input = GTK_WIDGET(gtk_builder_get_object(builder, "tax_input"));
+    tip_input = GTK_WIDGET(gtk_builder_get_object(builder, "tip_input"));
+    split_input = GTK_WIDGET(gtk_builder_get_object(builder, "split_input"));
+    result_tip = GTK_WIDGET(gtk_builder_get_object(builder, "result_tip"));
+
+    g_signal_connect(G_OBJECT(g_btn_cal_tip), "clicked", G_CALLBACK(tipCalculator), NULL);
+
+    gtk_widget_show_all (windowTip);
     gtk_main ();
 }
 
@@ -81,11 +151,17 @@ int main (int argc, char *argv[])
   window = GTK_WIDGET(gtk_builder_get_object(builder, "home"));
   gtk_builder_connect_signals(builder, NULL);
 
-  /*Open App1 btn*/
+  /*Open Discount App btn*/
   GtkWidget *g_btn_app1;
   g_btn_app1 = GTK_WIDGET(gtk_builder_get_object(builder, "btn_app1"));
   g_signal_connect(G_OBJECT(g_btn_app1), "clicked", G_CALLBACK(open_app1), NULL);
 
+
+
+  /*Open Tip App btn*/
+  GtkWidget *g_btn_tip;
+  g_btn_tip = GTK_WIDGET(gtk_builder_get_object(builder, "btn_tip"));
+  g_signal_connect(G_OBJECT(g_btn_tip), "clicked", G_CALLBACK(open_btn), NULL);
 
 
   /* Enter the main loop */
